737ba13cf38c792e7d565799a95ce633501792db
matevsh <mateuszmarcinkowski44@gmail.com>
2025-12-06 18:02:48 +0100

    feat: mock places data

--- src/features/dashboard/blank.tsx
+++ src/features/dashboard/blank.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export const Blank = () => {
-  return (
-    <div>Blank</div>
-  )
+  return <div>Blank</div>
 }
--- src/routes/swipe.tsx
+++ src/routes/swipe.tsx
@@ -1,27 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { PlaceCard } from '@/features/swipe/components/PlaceCard'
 import { ActionButtons } from '@/features/swipe/components/ActionButtons'
+import { PLACES_DATA } from '@/features/swipe/data.ts'
 
 export const Route = createFileRoute('/swipe')({
   component: RouteComponent,
 })
 
-const PLACE_DATA = {
-  name: 'Pałac Kultury i Nauki',
-  image:
-    'https://backend.triverna.pl/blog/wp-content/uploads/2023/11/Centrum-Bydgoszczy-z-charakterystycznymi-atrakcjami.jpeg',
-  description:
-    'Najwyższy budynek w Polsce i jeden z najbardziej rozpoznawalnych symboli Warszawy. Kompleks kulturalno-rozrywkowy z kinem, teatrami i punktami widokowymi.',
-  tags: ['Architektura', 'Kultura', 'Widok', 'Historia'],
-  location: 'Plac Defilad 1, Śródmieście',
-  distance: '2.3 km',
-}
-
 function RouteComponent() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="flex flex-col items-center gap-6">
-        <PlaceCard {...PLACE_DATA} />
+        <PlaceCard {...PLACES_DATA[2]} />
         <ActionButtons />
       </div>
     </div>
